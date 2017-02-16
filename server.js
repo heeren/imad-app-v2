1b3f1c7e8714682652ab61f26582a3b7b22e5e87
Heeren Darji <heerendarji@gmail.com>
2017-02-16 12:45:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+});
+
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
