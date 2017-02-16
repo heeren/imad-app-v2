2e68eb38b9e33897e97b727083061ffbb3b7fd74
Heeren Darji <heerendarji@gmail.com>
2017-02-16 17:25:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var articleOne = {
     
 };
 
-function creatTemplate(data){
+function createTemplate(data){
     var title = data.title;
     var heading = data.heading;
     var date = data.date;
